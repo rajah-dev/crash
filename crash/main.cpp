7057f9b252d21e9197b75f7698d605cca020b5d6
Vinoo Selvarajah <selv@rajah.dev>
2020-07-26 16:31:34 -0400

    Initial Commit

---
+++ crash/main.cpp
@@ -0,0 +1,14 @@
+// 
+// main.cpp
+// was created for crash in 2020
+// by Vinoo Selvarajah
+// gotta start somewhere ¯\_(ツ)_/¯ 
+// 
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
