e8678d961fbe8ee76821e04ca5b9813cdcf2ace4
Vinoo Selvarajah <selv@rajah.dev>
2020-08-03 16:44:09 -0400

    projected AABB is working, but need to reshuffle

--- crash/main.cpp
+++ crash/main.cpp
@@ -12,62 +12,58 @@
 
 #include "include/Game.h"
 
-struct vector {
-    float x;
-    float y;
+struct dynamicRect {
+    SDL_Rect rect;
+    SDL_FPoint velocity;
 };
+
 
 bool pointInRect( SDL_Point &point, SDL_Rect &rect ); //SDL_PointInRect dummy
 bool rectVsRect( SDL_Rect &rectA, SDL_Rect &rectB );
+bool rayVsRect( SDL_Point &rayOrigin, SDL_FPoint &rayDir, SDL_Rect &rect,
+               SDL_FPoint &contactPoint, SDL_Point &contactNormal, float &tHitNear);
 
-bool rayVsRect( SDL_Point &rayOrigin, SDL_Point &rayDir, SDL_Rect &rect,
-               SDL_Point contactPoint, SDL_Point contactNormal, int tHitNear)
+SDL_Point centerPoint( SDL_Rect rect )
 {
-    SDL_Point tNear = { 0, 0 };
-    tNear.x = ( rect.x - rayOrigin.x ) / rayDir.x;
-    tNear.y = ( rect.y - rayOrigin.y ) / rayDir.y;
-    
-    SDL_Point tFar = { 0, 0 };
-    tFar.x = ( rect.x + rect.w - rayOrigin.x ) / rayDir.x;
-    tFar.y = ( rect.y + rect.h - rayOrigin.y ) / rayDir.y;
-    
-    //if far point is closer than near point, because of origin's perspective relative to rectangle
-    //we need to sort them
-    if( tNear.x > tFar.x ) std::swap( tNear.x, tFar.x );
-    if( tNear.y > tFar.y ) std::swap( tNear.y, tFar.y );
-    
-    //simple checks to see if ray intersects if not (false)
-    if( tNear.x > tFar.y || tNear.y > tFar.x ) return false;
-    
-    //if it will intersect, find the nearest and furthest
-    tHitNear = std::max( tNear.x, tNear.y );
-    int tHitFar = std::min( tFar.x, tFar.y );
-    
-    //not interested if collision happens behind the rays direction
-    //implies closest hit point will also be negative
-    if( tHitFar < 0 ) return false;
-    
-    //can assume normal based on near.x vs near.y
-    
-    contactPoint.x = rayOrigin.x + tHitNear * rayDir.x;
-    contactPoint.y = rayOrigin.y + tHitNear * rayDir.y;
-    
-    if( tNear.x > tNear.y )
+    SDL_Point center;
+    center.x = rect.x + ( rect.w / 2 );
+    center.y = rect.y + ( rect.h / 2 );
+    return center;
+}
+
+SDL_FPoint centerFPoint( SDL_Rect rect )
+{
+    SDL_FPoint center;
+    center.x = rect.x + ( rect.w / 2 );
+    center.y = rect.y + ( rect.h / 2 );
+    return center;
+}
+
+bool rectVsRect( dynamicRect &dynamic, SDL_Rect &target, SDL_FPoint &contactPoint,
+                SDL_Point &contactNormal, float &collisionTime, float &elapsedTime )
+{
+    //assume rects don't start in collision, so if velocity is 0, just return false
+    if( dynamic.velocity.x == 0 && dynamic.velocity.y == 0 )
     {
-        if( rayDir.x < 0 )
-            contactNormal = { 1, 0 };
-        else
-            contactNormal = { -1, 0};
-    }
-    else if ( tNear.x < tNear.y )
-    {
-        if( rayDir.y < 0 )
-            contactNormal = { 0, 1 };
-        else
-            contactNormal = { 0, -1};
+        return false;
     }
     
-    return true;
+    //expand size of target rectangle to include buffer, based on dynamic rectangle
+    SDL_Rect expandedTarget;
+    expandedTarget.x = target.x - ( dynamic.rect.w / 2 );
+    expandedTarget.y = target.y - ( dynamic.rect.h / 2 );
+    expandedTarget.w = target.w + dynamic.rect.w;
+    expandedTarget.h = target.h + dynamic.rect.h;
+    
+    if( rayVsRect( centerPoint( dynamic.rect ), dynamic.velocity, expandedTarget, contactPoint, contactNormal, collisionTime, elapsedTime ) )
+    {
+        if( collisionTime <= 1.0f ) //
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 int main(int argc, const char * argv[]) {
@@ -81,18 +77,25 @@ int main(int argc, const char * argv[]) {
     {
         SDL_Event event;
         bool hasQuit = false;
-        SDL_Point originPos = { 10, 10 };
+        
+        SDL_Point originPos = { 550, 400 };
         SDL_Point mousePos = { 0, 0 };
-        SDL_Point rayDirection = { 0, 0 };
-        SDL_Point cPoint = { 0, 0 };
+        
+        SDL_FPoint rayDirection = { 0, 0 };
+        SDL_FPoint cPoint = { 0.0, 0.0 };
         SDL_Point cNormal = { 0, 0 };
-        int t = 0;
-        SDL_Color gColor = { 113, 28, 145, 255 };
-        SDL_Color oColor = { 0, 0, 0, 255 };
-        SDL_Color pColor = { 255, 255, 255, 255 };
+        float time = 0;
+        
+        SDL_Color gColor = { 0x33, 0x13, 0x5C, 255 };
+        SDL_Color obColor = { 0xDE, 0x38, 0xC8, 255 };
+        SDL_Color highlight = { 0xFF, 0xD3, 0x00, 255 };
+        
         SDL_Rect obstacle = { 300, 200, 50, 50 };
-        SDL_Rect player = { 250, 150, 50, 50 };
-        int velocity = 1;
+        SDL_FRect hit = { cPoint.x, cPoint.y, 5, 5 };
+        
+
+        
+        
         while( !hasQuit )
         {
             while( SDL_PollEvent( &event ) )
@@ -114,27 +117,38 @@ int main(int argc, const char * argv[]) {
                     }
                 }
             }
-            oColor = { 255, 255, 255, 255 };
-            //update
+            obColor = { 255, 255, 255, 255 };
+            
+            //A ray is P (origin)  and a D(irection vector) * T(ime)
+            //Then D (rayDir) = Q (endpoint) - P (origin)
             rayDirection.x = mousePos.x - originPos.x;
             rayDirection.y = mousePos.y - originPos.y;
-            
-            if( rectVsRect( player, obstacle ) )
-            {
-                oColor = { 0, 0, 0, 255 };
-            }
-            
+            hit.x = cPoint.x;
+            hit.y = cPoint.y;
+        
             //draw
             
             SDL_SetRenderDrawColor( gRenderer, gColor.r, gColor.g, gColor.b, gColor.a );
             SDL_RenderClear( gRenderer );
             
-            SDL_SetRenderDrawColor( gRenderer, oColor.r, oColor.g, oColor.b, oColor.a );
+            SDL_SetRenderDrawColor( gRenderer, highlight.r, highlight.g, highlight.b, highlight.a );
+            SDL_RenderDrawLine( gRenderer, originPos.x, originPos.y, mousePos.x, mousePos.y);
+            
+            if( rayVsRect(originPos, rayDirection, obstacle, cPoint, cNormal, time) && time <= 1.0f )
+            {
+                SDL_SetRenderDrawColor( gRenderer, obColor.r, obColor.g, obColor.b, obColor.a );
+                SDL_RenderFillRectF( gRenderer, &hit );
+                SDL_RenderDrawLine( gRenderer, cPoint.x, cPoint.y, cPoint.x + cNormal.x*20, cPoint.y + cNormal.y*20 );
+                
+                SDL_SetRenderDrawColor( gRenderer, highlight.r, highlight.g, highlight.b, highlight.a );
+            }
+            else
+            {
+                SDL_SetRenderDrawColor( gRenderer, obColor.r, obColor.g, obColor.b, obColor.a );
+            }
             SDL_RenderFillRect( gRenderer, &obstacle );
             
-            if( rayVsRect(originPos, rayDirection, obstacle, cPoint, cNormal, t)) SDL_SetRenderDrawColor( gRenderer, 0, 0, 0, 255 );
-            else SDL_SetRenderDrawColor( gRenderer, 255, 0, 0, 255 );
-            SDL_RenderDrawLine( gRenderer, originPos.x, originPos.y, mousePos.x, mousePos.y);
+
 
             
             //SDL_RenderDrawLine( gRenderer, originPos.x, originPos.y, mousePos.x, mousePos.y);
@@ -164,5 +178,56 @@ bool rectVsRect( SDL_Rect &rectA, SDL_Rect &rectB )
             rectA.y + rectA.h > rectB.y ); //Is A's bottom below B's top?
 }
 
-
+//possibly may want all of these to be float vectors
+bool rayVsRect( SDL_Point &rayOrigin, SDL_FPoint &rayDir, SDL_Rect &rect,
+               SDL_FPoint &contactPoint, SDL_Point &contactNormal, float &tHitNear)
+{
+    //Calculate the collision point nearest to the ray origin, based on x, y axis
+    //A ray is P (origin)  and a D(irection vector) * T(ime 0 - 1)
+    SDL_FPoint tNear = { 0, 0 };
+    tNear.x = ( rect.x - rayOrigin.x ) / rayDir.x;
+    tNear.y = ( rect.y - rayOrigin.y ) / rayDir.y;
+    
+    //Calculate the collision point furthers from the ray origin, based on x, y axis
+    SDL_FPoint tFar = { 0, 0 };
+    tFar.x = ( rect.x + rect.w - rayOrigin.x ) / rayDir.x;
+    tFar.y = ( rect.y + rect.h - rayOrigin.y ) / rayDir.y;
+    
+    //if far point is closer than near point, because of origin's perspective relative to rectangle
+    //we need to sort them
+    if( tNear.x > tFar.x ) std::swap( tNear.x, tFar.x );
+    if( tNear.y > tFar.y ) std::swap( tNear.y, tFar.y );
+    
+    //simple checks to see if ray intersects, if not (false)
+    if( tNear.x > tFar.y || tNear.y > tFar.x ) return false;
+    
+    //if it will intersect, find the nearest and furthest
+    tHitNear = std::max( tNear.x, tNear.y );
+    float tHitFar = std::min( tFar.x, tFar.y );
+    
+    //not interested if collision happens behind the rays direction
+    //implies closest hit point will also be negative
+    if( tHitFar < 0 ) return false;
+    
+    //can assume normal based on near.x vs near.y
+    contactPoint.x = rayOrigin.x + tHitNear * rayDir.x;
+    contactPoint.y = rayOrigin.y + tHitNear * rayDir.y;
+    
+    if( tNear.x > tNear.y )
+    {
+        if( rayDir.x < 0 )
+            contactNormal = { 1, 0 };
+        else
+            contactNormal = { -1, 0};
+    }
+    else if ( tNear.x < tNear.y )
+    {
+        if( rayDir.y < 0 )
+            contactNormal = { 0, 1 };
+        else
+            contactNormal = { 0, -1};
+    }
+    
+    return true;
+}
 
